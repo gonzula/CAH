4791c089254a76d5e9175e7bd42f2ec7303fdee5
Gonzo <carloseaf@gmail.com>
2018-10-23 22:45:02 -0300

    Fix card deck logic

--- model.py
+++ model.py
@@ -225,7 +225,7 @@ class Card:
     def draw_cards(deck, n):
         deck = deck[:]
         np.random.shuffle(deck)
-        deck = sorted(deck, reverse=True)
+        deck = sorted(deck, reverse=False)
 
         p = np.arange(len(deck)).astype('double')
         p /= p.sum()
